cfea673c95b13a9e4f59ba46174b54560bcbd9de
nqh1089 <huynq0503@gmail.com>
2026-02-23 00:44:17 +0700

    Admin: Quản lý Nhân viên - List fix cứng

---
+++ src/data.js
@@ -0,0 +1,25 @@
+// src/data.js
+export const mockEmployees = [
+  {
+    id: 1,
+    account: 'nqh1089',
+    name: 'Nguyễn Quang Huy',
+    role: 'Manager',
+    phone: '086886886',
+    email: 'huynq@rys',
+    active: true,
+    startDate: '2026-01-26',
+    password: '1',
+  },
+  {
+    id: 2,
+    account: 'ad',
+    name: 'Test',
+    role: 'Nhân viên',
+    phone: '0123',
+    email: 'g@gm',
+    active: true,
+    startDate: '2026-02-02',
+    password: '123',
+  },
+]
